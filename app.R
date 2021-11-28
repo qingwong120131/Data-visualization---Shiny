2881eb2c1f8f873632802be630f0594776f96877
qingwong120131 <85275874+qingwong120131@users.noreply.github.com>
2021-11-27 22:07:35 -0800

    Update app.R

--- app.R
+++ app.R
@@ -6,8 +6,12 @@ library(ggthemes)
 library(tidyverse)
 library(scales)
 library(shinyjs)
+library(ggpubr)
 
-#----QW 7/21/21 - USING PRISM THEME ----
+
+
+#----QW 08/16/21 SHOWING TONS OF DATA WITH TABS
+#----QW 11/27/21 QOL changes
 
 #-----helper function - for building scatter plots with line---------------
 plot_pointswithline <- function(data,x,y,groupby,colorby,shapeby,panelby){
@@ -16,7 +20,7 @@ plot_pointswithline <- function(data,x,y,groupby,colorby,shapeby,panelby){
       geom_point(size = 5) +
       theme_prism(
         palette = "black_and_white",
-        base_size = 20
+        base_size = 16
       ) +
       theme(legend.position = "right") +
       #theme(text = element_text(size = 20),
@@ -30,72 +34,43 @@ plot_pointswithline <- function(data,x,y,groupby,colorby,shapeby,panelby){
   return(p)
 }
 
+#-----helper function - for forcing integer x axis breaks on the dashboard---------------
+
+integer_breaks <- function(n = 5, ...) {
+  fxn <- function(x) {
+    breaks <- floor(pretty(x, n, ...))
+    names(breaks) <- attr(breaks, "labels")
+    breaks
+  }
+  return(fxn)
+}
+
 # User interface ----
 ui <- fluidPage(
   useShinyjs(),
   sidebarLayout(
     sidebarPanel(
-      actionButton(
-        inputId = "load",
-        label = "Browse data (.xlsx only)"
-      ),
+      width = 2,
       
-      actionButton(
-        inputId = "reload",
-        label = "Load/reload"
-      ),
+               actionButton(
+                 inputId = "load",
+                 label = "Browse data (.xlsx)",
+                 #width = "75%"
+               
+               ),
+        
+               actionButton(
+                 inputId = "reload",
+                 #icon("paper-plane"),
+                 label = "Load selected file",
+                 #width = "25%"
+               ),
+               
+     
+      
       br(),
       br(),
-      selectInput(inputId = "select_sheet",
-                  label = "Select sheet",
-                  choices = NULL),
-      selectInput(
-        inputId = "selectinput_x",
-        label = "Select x-axis:",
-        choices = NULL),
-      splitLayout(
-        textInput(
-          inputId = "x_min", 
-          label = NULL, 
-          value = "", 
-          #width = "600px", 
-          placeholder = "X min"
-        ),
-        textInput(
-          inputId = "x_max", 
-          label = NULL, 
-          value = "", 
-          #width = "600px", 
-          placeholder = "X max"
-        ),
-        actionButton(
-          inputId = "reset_x",
-          label = "Default"
-        )
-      ),
-      selectInput(
-        inputId = "selectinput_y",
-        label = "Select y-axis:",
-        choices = NULL),
-      splitLayout(
-        textInput(
-          inputId = "y_min", 
-          label = NULL, 
-          value = "", 
-          #width = "200px", 
-          placeholder = "Y min"
-        ),
-        textInput(
-          inputId = "y_max", 
-          label = NULL, 
-          value = "", 
-          #width = "200px", 
-          placeholder = "Y max"
-        ),
-        actionButton(
-          inputId = "reset_y",
-          label = "Default")
-      ),
+
       actionButton(
         inputId = "save",
         label = "Save inputs"
@@ -104,30 +79,13 @@ ui <- fluidPage(
         inputId = "restore",
         label = "Restore inputs"
       ),
-    ),  
-    mainPanel(
+      actionButton(inputId = "refresh",icon = icon("refresh"),label = "Refresh Plot"),
       br(),
-      tabsetPanel(
-        # tabPanel("Growth", actionButton(inputId = "refresh",label = "Go"),
-        #          fluidRow(12,
-        #                   column(4,plotOutput(outputId = "vcd_plot")),
-        #                   column(4,plotOutput(outputId = "vcd_plot")),
-        #                   column(4,plotOutput(outputId = "vcd_plot"))
-        #          ),
-        #          # fluidRow(12,
-        #          #          column(6,plotOutput(outputId = "gluc_plot")),
-        #          #          column(6,plotOutput(outputId = "lac_plot")),
-        #          #          #column(4,plotOutput(outputId = "foldexp_plot"))
-        #          # )
-        # ),
-        tabPanel("Plot", actionButton(inputId = "refresh",label = "Go"), plotOutput(outputId = "scatter")),
-        tabPanel("Table", tableOutput(outputId = "table"))
-      )
-    )
-  ),
-  
-  sidebarLayout(
-    sidebarPanel(
+      br(),
+      
+      selectInput(inputId = "select_sheet",
+                  label = "Select sheet",
+                  choices = NULL),
       selectInput(
         inputId = "selectinput_color",
         label = "Color by:",
@@ -152,21 +110,91 @@ ui <- fluidPage(
         inputId = "selectinput_line3",
         label = "Line by:",
         choices = NULL)
-    ),
+    ),  
     mainPanel(
-      actionButton("add_btn", "Add Filter"),
-      actionButton("rm_btn", "Remove Filter"),
-      uiOutput(outputId = "filters"),
-      uiOutput(outputId = "select_all"),
-      uiOutput(outputId = "deselect_all"),
-      uiOutput(outputId = "values")
+      width = 9,
+      br(),
+      tabsetPanel(
+        tabPanel("Free Plot", 
+                 fluidRow(
+                   br(),
+                   column(3,
+                          selectInput(
+                            inputId = "selectinput_x",
+                            label = "Select x-axis:",
+                            choices = NULL),
+                          splitLayout(
+                            textInput(
+                              inputId = "x_min", 
+                              label = NULL, 
+                              value = "", 
+                              #width = "600px", 
+                              placeholder = "X min"
+                            ),
+                            textInput(
+                              inputId = "x_max", 
+                              label = NULL, 
+                              value = "", 
+                              #width = "600px", 
+                              placeholder = "X max"
+                            ),
+                            actionButton(
+                              inputId = "reset_x",
+                              label = "Default"
+                            )
+                          ),
+                          selectInput(
+                            inputId = "selectinput_y",
+                            label = "Select y-axis:",
+                            choices = NULL),
+                          splitLayout(
+                            textInput(
+                              inputId = "y_min", 
+                              label = NULL, 
+                              value = "", 
+                              #width = "200px", 
+                              placeholder = "Y min"
+                            ),
+                            textInput(
+                              inputId = "y_max", 
+                              label = NULL, 
+                              value = "", 
+                              #width = "200px", 
+                              placeholder = "Y max"
+                            ),
+                            actionButton(
+                              inputId = "reset_y",
+                              label = "Default")
+                          )
+                          
+                   ),
+                   column(9,plotOutput(outputId = "scatter"
+                                       #height="800px"
+                                       ))
+                  )
+                 ),
+        tabPanel("BR Growth",
+
+                 plotOutput('growth',height="600px", width = "100%")),
+        tabPanel("BR Metabolites",
+
+                 plotOutput('metabolites',height="600px", width = "100%")),
+        tabPanel("BR Phenotype",
+
+                 plotOutput('phenotype',height="600px", width = "100%")),
+        tabPanel("Table", tableOutput(outputId = "table")),
+        tabPanel("Back-end", )
+      )
     )
   ),
-  splitLayout(
-    tableOutput('show_inputs'),
-    tableOutput('show_inputs2'),
-    tableOutput('lastselect')
-  ),
+  actionButton("add_btn", "Add Filter"),
+  actionButton("rm_btn", "Remove Filter"),
+  uiOutput(outputId = "filters"),
+  uiOutput(outputId = "select_all"),
+  uiOutput(outputId = "deselect_all"),
+  uiOutput(outputId = "values"),
+  tableOutput(outputId = "show_inputs"),
+  tableOutput(outputId = "show_inputs2"),
 )
 
 # Server logic ----
@@ -303,7 +331,18 @@ server <- function(input, output, session) {
   
   df <- reactive({
     if (is.null(data()) == FALSE)
-      data()[[input$select_sheet]]
+      df <- data()[[input$select_sheet]]
+    if (any(df[1,] %in% c("Character","Numeric","Integer")) == TRUE)
+      for (i in c(1:ncol(df))){
+              if (df[[1,i]] == "Integer")
+                df[[i]] =  as.integer(df[[i]])
+              else if (df[[1,i]] == "Character")
+                df[[i]] =  as.character(df[[i]])
+              else if (df[[1,i]] == "Numeric")
+                df[[i]] =  as.numeric(df[[i]])
+            }
+      df <- df[-1,]
+    return(df)
   })
   
   new_df <- reactive({
@@ -542,9 +581,15 @@ server <- function(input, output, session) {
       else y_max <- default_ranges(p)[[4]]
       
       p <- p+coord_cartesian(xlim = c(x_min,x_max),ylim=c(y_min,y_max))
+        
+      if (is.integer(filtered_data()[[input$selectinput_x]]) == TRUE){
+        p <- p + scale_x_continuous(breaks = integer_breaks())
+      }
       
-      if (is.numeric(filtered_data()[[input$selectinput_x]]) == TRUE)
+      else if (is.numeric(filtered_data()[[input$selectinput_x]])){
         p <- p + scale_x_continuous(breaks = scales::pretty_breaks(n = 10))
+      }
+
       if (is.numeric(filtered_data()[[input$selectinput_y]]) == TRUE)
         p <- p + scale_y_continuous(breaks = scales::pretty_breaks(n = 10))
       return(p)
@@ -552,34 +597,89 @@ server <- function(input, output, session) {
       )
       })
   
-  # output$vcd_plot <- renderPlot({
-  #   req(input$refresh)
-  #   isolate({
-  #     if (is.null(new_df()[['combined_line']]) == FALSE)
-  #       p <- plot_pointswithline(data = isolate(filtered_data()), x = isolate(input$selectinput_x), y = "Viable Count (e6/mL)",groupby="combined_line",colorby=isolate(input$selectinput_color),shapeby=isolate(input$selectinput_shape),panelby=isolate(input$selectinput_panel))
-  #     else
-  #       p<- plot_pointswithline(data = isolate(filtered_data()), x = isolate(input$selectinput_x), y = "Viable Count (e6/mL)",groupby="NULL",colorby=isolate(input$selectinput_color),shapeby=isolate(input$selectinput_shape),panelby=isolate(input$selectinput_panel))
-  #     # 
-  #     # if (input$x_min!="") x_min <- as.numeric(input$x_min)
-  #     # else x_min <- default_ranges(p)[[1]]
-  #     # if (input$x_max!="") x_max <- as.numeric(input$x_max)
-  #     # else x_max <- default_ranges(p)[[2]]
-  #     # if (input$y_min!="") y_min <- as.numeric(input$y_min)
-  #     # else y_min <- default_ranges(p)[[3]]
-  #     # if (input$y_max!="") y_max <- as.numeric(input$y_max)
-  #     # else y_max <- default_ranges(p)[[4]]
-  #     
-  #     # p <- p+coord_cartesian(xlim = c(x_min,x_max),ylim=c(y_min,y_max))
-  #     # 
-  #     # if (is.numeric(filtered_data()[[input$selectinput_x]]) == TRUE)
-  #     #   p <- p + scale_x_continuous(breaks = scales::pretty_breaks(n = 10))
-  #     # if (is.numeric(filtered_data()[[input$selectinput_y]]) == TRUE)
-  #     #   p <- p + scale_y_continuous(breaks = scales::pretty_breaks(n = 10))
-  #     # return(p)
-  #   }
-  #   )
-  # })
-  # 
+
+  growth_plots <- list(
+                        "Viable Count (e6/mL)",
+                      "% Viability",
+                      "NC-200 Fold Expansion"
+                      )
+  
+  met_plots <- list(
+                       "Metaflex pH",
+                       "Metaflex Lactate (mM/L)",
+                       "Metaflex Glucose (mg/dL)"
+  )
+  
+  phenotype_plots <- list("DAPIneg (%)",
+                          "ACR+/CD56+ (%)"
+                          # "NKG2A+/CD16- (%)",
+                          # "NKG2A+/CD16+ (%)",
+                          # "NKG2A-/CD16+ (%)"
+                          )
+  
+  output$growth <- renderPlot({
+      req(input$refresh)
+      isolate({
+        plots <- lapply(seq_len(length(growth_plots)),function(i){
+          if (is.null(new_df()[['combined_line']]) == FALSE)
+              p <- plot_pointswithline(data = isolate(filtered_data()), x = "Day", y = growth_plots[[i]], groupby="combined_line",colorby=isolate(input$selectinput_color),shapeby=isolate(input$selectinput_shape),panelby=isolate(input$selectinput_panel))
+          else
+              p<- plot_pointswithline(data = isolate(filtered_data()), x = "Day", y = growth_plots[[i]], groupby="NULL",colorby=isolate(input$selectinput_color),shapeby=isolate(input$selectinput_shape),panelby=isolate(input$selectinput_panel))
+          
+          p <- p + scale_x_continuous(breaks = integer_breaks())
+          p <- p + scale_y_continuous(breaks = scales::pretty_breaks(n = 10))
+        })
+        ggarrange(plotlist = plots, common.legend = TRUE, legend="right")
+      })
+    })
+  
+  output$metabolites <- renderPlot({
+    req(input$refresh)
+    isolate({
+      plots <- lapply(seq_len(length(met_plots)),function(i){
+        if (is.null(new_df()[['combined_line']]) == FALSE)
+          p <- plot_pointswithline(data = isolate(filtered_data()), x = "Day", y = met_plots[[i]], groupby="combined_line",colorby=isolate(input$selectinput_color),shapeby=isolate(input$selectinput_shape),panelby=isolate(input$selectinput_panel))
+        else
+          p<- plot_pointswithline(data = isolate(filtered_data()), x = "Day", y = met_plots[[i]], groupby="NULL",colorby=isolate(input$selectinput_color),shapeby=isolate(input$selectinput_shape),panelby=isolate(input$selectinput_panel))
+        
+        p <- p + scale_x_continuous(breaks = integer_breaks())
+        p <- p + scale_y_continuous(breaks = scales::pretty_breaks(n = 10))
+      })
+      ggarrange(plotlist = plots, common.legend = TRUE, legend="right")
+    })
+  })
+  
+  output$phenotype <- renderPlot({
+    req(input$refresh)
+    isolate({
+      plots <- lapply(seq_len(length(phenotype_plots)),function(i){
+        if (is.null(new_df()[['combined_line']]) == FALSE)
+          p <- plot_pointswithline(data = isolate(filtered_data()), x = "Day", y = phenotype_plots[[i]], groupby="combined_line",colorby=isolate(input$selectinput_color),shapeby=isolate(input$selectinput_shape),panelby=isolate(input$selectinput_panel))
+        else
+          p<- plot_pointswithline(data = isolate(filtered_data()), x = "Day", y = phenotype_plots[[i]], groupby="NULL",colorby=isolate(input$selectinput_color),shapeby=isolate(input$selectinput_shape),panelby=isolate(input$selectinput_panel))
+        
+        p <- p + scale_x_continuous(breaks = integer_breaks())
+        p <- p + scale_y_continuous(breaks = scales::pretty_breaks(n = 10))
+      })
+      ggarrange(plotlist = plots, common.legend = TRUE, legend="right")
+    })
+  })
+   
+  # lapply(seq_len(length(list_of_plots)), function(i)
+  #   output[[names(list_of_plots)[[i]]]] <- renderPlot({
+  #     req(input$refresh)
+  #     isolate({
+  #       if (is.null(new_df()[['combined_line']]) == FALSE)
+  #         p <- plot_pointswithline(data = isolate(filtered_data()), x = "Day", y = list_of_plots[[i]], groupby="combined_line",colorby=isolate(input$selectinput_color),shapeby=isolate(input$selectinput_shape),panelby=isolate(input$selectinput_panel))
+  #       else
+  #         p<- plot_pointswithline(data = isolate(filtered_data()), x = "Day", y = list_of_plots[[i]], groupby="NULL",colorby=isolate(input$selectinput_color),shapeby=isolate(input$selectinput_shape),panelby=isolate(input$selectinput_panel))
+  #       p <- p + scale_x_continuous(breaks = scales::pretty_breaks(n = 10))
+  #       p <- p + scale_y_continuous(breaks = scales::pretty_breaks(n = 10))
+  #       return(p)
+  #     })
+  #   })
+  # )
+  
 
   output$table <- renderTable({
     filtered_data()
@@ -589,7 +689,7 @@ server <- function(input, output, session) {
     AllInputs()
     #default_ranges(plot_pointswithline(data = filtered_data(), x = input$x, y = input$y,groupby="combined_line",colorby=input$color))
   })
-  
+
   output$show_inputs2 <- renderTable({
     current_inputs()
   })
